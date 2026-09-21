37946215276636b663ed5fbff238d7cab59f27a5
d01226091-wq <d01226091@gmail.com>
2026-09-22 00:15:54 +0300

    Add client sky color mixin

---
+++ src/main/java/ru/tysmvisuals/mixin/ClientWorldSkyColorMixin.java
@@ -0,0 +1,21 @@
+package ru.tysmvisuals.mixin;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.util.math.Vec3d;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import ru.tysmvisuals.TysmVisualsClient;
+
+@Mixin(ClientWorld.class)
+public class ClientWorldSkyColorMixin {
+    @Inject(method = "getSkyColor", at = @At("HEAD"), cancellable = true)
+    private void tysmvisuals$customSkyColor(Vec3d cameraPos, float tickProgress,
+                                             CallbackInfoReturnable<Integer> cir) {
+        if (TysmVisualsClient.isSkyColorEnabled()) {
+            cir.setReturnValue(TysmVisualsClient.getSkyColor());
+        }
+    }
+}
