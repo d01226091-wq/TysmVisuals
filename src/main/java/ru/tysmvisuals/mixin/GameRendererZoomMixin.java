d8712fa91b0c6a197f5cc3927680eba4cac4c094
d01226091-wq <d01226091@gmail.com>
2026-09-22 16:29:54 +0300

    Fix zoom mixin without MixinExtras

--- src/main/java/ru/tysmvisuals/mixin/GameRendererZoomMixin.java
+++ src/main/java/ru/tysmvisuals/mixin/GameRendererZoomMixin.java
@@ -4,13 +4,17 @@ import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.GameRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyReturnValue;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import ru.tysmvisuals.TysmVisualsClient;
 
 @Mixin(GameRenderer.class)
 public class GameRendererZoomMixin {
-    @ModifyReturnValue(method = "getFov", at = @At("RETURN"))
-    private float tysmvisuals$zoom(float fov) {
-        return TysmVisualsClient.isZoomActive() ? fov * 0.35f : fov;
+    @Inject(method = "getFov", at = @At("RETURN"), cancellable = true)
+    private void tysmvisuals$zoom(Camera camera, float tickDelta, boolean changingFov,
+                                   CallbackInfoReturnable<Float> cir) {
+        if (TysmVisualsClient.isZoomActive()) {
+            cir.setReturnValue(cir.getReturnValue() * 0.35f);
+        }
     }
 }
