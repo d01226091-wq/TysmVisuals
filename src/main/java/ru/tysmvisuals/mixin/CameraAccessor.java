e8c2a379e6e62ed140cf95b9eb256ac1735b9f74
d01226091-wq <d01226091@gmail.com>
2026-09-22 16:29:56 +0300

    Add camera rotation invoker for freelook

---
+++ src/main/java/ru/tysmvisuals/mixin/CameraAccessor.java
@@ -0,0 +1,11 @@
+package ru.tysmvisuals.mixin;
+
+import net.minecraft.client.render.Camera;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(Camera.class)
+public interface CameraAccessor {
+    @Invoker("setRotation")
+    void tysmvisuals$setRotation(float yaw, float pitch);
+}
